56a7e1b101c5225c7abc360a75a823d7f5c5a4ca
Careen Joseph <careen840@gmail.com>
2024-05-07 19:52:20 +0200

    new

--- basic_scrapy_spider/spiders/quotes.py
+++ basic_scrapy_spider/spiders/quotes.py
@@ -7,7 +7,19 @@ class LinkedJobsSpider(scrapy.Spider):
     def start_requests(self):
         first_job_on_page = 0
         first_url = self.api_url + str(first_job_on_page)
-        yield scrapy.Request(url=first_url, callback= self.parse_job, meta{'first_job_on_page':first_job_on_page})
+        yield scrapy.Request(url=first_url, callback= self.parse_job, meta={'first_job_on_page':first_job_on_page})
     
     def parse_job(self, response):
         first_job_on_page= response.meta['first_job_on_page']
+        
+        jobs = response.css("li")
+        
+        for jobs in jobs:
+            job_item['job_title']= job.csss("h3::text").get(default='not-found').strip()
+            job_item['job_detail_url']= job.csss(".base-card_full_link::attr(href)").get(default='not-found').strip()
+            job_item['job_LISTED']= job.csss("time::text").get(default='not-found').strip()
+            
+            job_item['company_name']= job.csss("h4 a::text").get(default='not-found').strip()
+            job_item['company_link']= job.csss("h4 a::attr(href)").get(default='not-found').strip()
+            job_item['company_location']= job.csss(".job-search-card_location::text").get(default='not-found').strip()
+            yield job_item
