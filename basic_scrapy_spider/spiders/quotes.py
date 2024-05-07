5f23c712ad563422e8d99ef6f37d85eb25121ff5
Careen Joseph <careen840@gmail.com>
2024-05-07 18:38:13 +0200

    new

--- basic_scrapy_spider/spiders/quotes.py
+++ basic_scrapy_spider/spiders/quotes.py
@@ -2,4 +2,9 @@ import scrapy
 
 class LinkedJobsSpider(scrapy.Spider):
     name= "linkedin_jobs"
+    api_url= 'https://www.linkedin.com/jobs/search/?currentJobId=3918380402&distance=25&geoId=100506914&keywords=junior%20marketing%20manager&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true&start=25'
     
+    def start_requests(self):
+        first_job_on_page = 0
+        first_url = self.api_url + str(first_job_on_page)
+        yield scrapy.Request(url=first_url, callback= self.parse_job, meta{'first_job_on_page':first_job_on_page})
